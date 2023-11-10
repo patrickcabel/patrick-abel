69f92af61d34ca2e3605d098cdd42b3d337557d0
patrickAbel <pabelnwa@gmail.com>
2023-11-09 20:32:11 -0700

    edited the javascript to render the pages dynamically

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll("nav a");
+    const contentSection = document.getElementById("content");
 
-    // Function to remove the "active" class from all navigation links
-    function removeActiveClass() {
-        navLinks.forEach((link) => link.classList.remove("active"));
+    // Function to load content dynamically
+    function loadContent(sectionId) {
+        fetch(sectionId + '.html')
+            .then(response => response.text())
+            .then(data => {
+                contentSection.innerHTML = data;
+            })
+            .catch(error => {
+                console.error("Error loading content: " + error);
+            });
     }
 
-    // Event listeners for each section
-    sections.forEach((section) => {
-        section.addEventListener("click", function () {
-            removeActiveClass();
+    // Initial load of the "About" section
+    loadContent("#about");
+
+    // Add click event listeners to the navigation links
+    navLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            // Remove the "active" class from all links
+            navLinks.forEach(navLink => navLink.classList.remove("active"));
+
+            // Add the "active" class to the clicked link
             this.classList.add("active");
 
-            // Get the section's corresponding navigation link and make it active
-            const link = document.querySelector(`a[href="#${section.id}"]`);
-            if (link) {
-                link.classList.add("active");
-            }
+            // Load the content for the clicked section
+            const targetId = this.getAttribute("href");
+            loadContent(targetId);
         });
     });
-
-    // Highlight the "active" navigation link when the page loads
-    const currentSection = sections[0];
-    currentSection.classList.add("active");
-    const currentLink = document.querySelector(`a[href="#${currentSection.id}"]`);
-    if (currentLink) {
-        currentLink.classList.add("active");
-    }
 });
