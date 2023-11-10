7b24d226b4f851332cbb724be740a3926453656c
patrickAbel <pabelnwa@gmail.com>
2023-11-09 20:29:20 -0700

    edited all pages'

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll("nav a");
 
-    navLinks.forEach((link) => {
-        link.addEventListener("click", function (e) {
-            // Prevent the default link behavior
-            e.preventDefault();
+    // Function to remove the "active" class from all navigation links
+    function removeActiveClass() {
+        navLinks.forEach((link) => link.classList.remove("active"));
+    }
 
-            // Remove the "active" class from all links
-            navLinks.forEach((navLink) => navLink.classList.remove("active"));
-
-            // Add the "active" class to the clicked link
+    // Event listeners for each section
+    sections.forEach((section) => {
+        section.addEventListener("click", function () {
+            removeActiveClass();
             this.classList.add("active");
 
-            // Scroll to the section based on the href attribute
-            const targetId = this.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
-            targetSection.scrollIntoView({
-                behavior: "smooth",
-            });
+            // Get the section's corresponding navigation link and make it active
+            const link = document.querySelector(`a[href="#${section.id}"]`);
+            if (link) {
+                link.classList.add("active");
+            }
         });
     });
+
+    // Highlight the "active" navigation link when the page loads
+    const currentSection = sections[0];
+    currentSection.classList.add("active");
+    const currentLink = document.querySelector(`a[href="#${currentSection.id}"]`);
+    if (currentLink) {
+        currentLink.classList.add("active");
+    }
 });
